e80d67b5845c03d6549be2eb37ce9f09e88963d9
--add <julian.eduardo.aquino@gmail.com>
2024-12-06 20:55:25 -0300

    se generan las rutas de usuarios y productos

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/home', function(req, res, next) {
   res.render('home');
 });
 
+router.post('/submit', function(req, res, next) {
+  const { username,email,password,dob,genero,country} = req.body;
+  console.log(req.body);
+  
+  const respuesta = {
+    id:1,
+    nombre:username,
+    email,
+  }
+
+  res.send(respuesta)
+  
+});
+
 module.exports = router;
