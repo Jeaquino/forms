ff2afe9e601e58adf64e0b0666ef95c533d5b59a
julian E. Aquino <julian.aquino@cencosud.com.ar>
2024-12-11 09:25:49 -0300

    se agregan tags de ejs para el renderizado dinamico de las vistas

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -7,17 +7,22 @@ const productsController = {
         const bicicleta = bicicletas.find(element => element.id == id);
         console.log(bicicleta);
         if(bicicleta){
-            res.send(bicicleta);
+            res.render('products/detail',{bicicleta});
         }else{
             res.send("El producto seleccionado no existe");
         }
     },
+
     all:(req,res,next)=>{
         const {marca,color,velocidades} = req.query;
         
-        const bicicletas = JSON.parse(fs.readFileSync('./data/bicicletas.json', 'utf-8'));
-        const resultado = bicicletas.filter(element => element.marca == marca || element.color == color || element.velocidades == velocidades);        
-        res.send(resultado)
+        let bicicletas = JSON.parse(fs.readFileSync('./data/bicicletas.json', 'utf-8'));
+        
+        if(marca || color || velocidades){
+            bicicletas = bicicletas.filter(element => element.marca == marca || element.color == color || element.velocidades == velocidades);        
+        }
+        
+        res.render('products/all',{bicicletas})
     }
 }
 
