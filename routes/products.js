e97ca52af830826f97eb84b28a2fdd001ad7d740
--add <julian.eduardo.aquino@gmail.com>
2024-12-13 18:17:50 -0300

    se agrega el input de busqueda en el header

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {detail,all} = require('../controllers/productsController')
 
 router.get('/',all);
+router.post('/',all);
 router.get('/:id/detail',detail);
 
 
