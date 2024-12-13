bac2d28edeb252cae51fcaeaa0c0dbc9235d7bb0
--add <julian.eduardo.aquino@gmail.com>
2024-12-13 20:46:56 -0300

    se agregan enlaces y la vista de detalle de productos

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+const {home} = require('../controllers/indexController')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/home', function(req, res, next) {
-  res.render('home');
-});
+router.get('/home', home);
 
 router.post('/submit', function(req, res, next) {
   const { username,email,password,dob,genero,country} = req.body;
